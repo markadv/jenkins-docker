649a8e9dcdc264cff771d75f986d025644e9a756
Mark Timothy Advento <mark@adventodesign.com>
2022-09-17 22:07:20 +0800

    Jenkins PR Trigger

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -20,6 +20,6 @@ describe("Survey Model", function () {
         expect(result).to.equal("Error! Captcha input doesn't matched.");
     });
     it("Fake test for Jenkins test.", function () {
-        expect(3).to.equal(3);
+        expect(2).to.equal(2);
     });
 });
